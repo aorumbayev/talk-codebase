2969d5bc4f5199409a095c55f51a94f76b65add6
Altynbek Orumbayev <aorumbayev@protonmail.com>
2023-06-01 11:48:28 +0200

    feat: tweaking codebase

--- talk_codebase/cli.py
+++ talk_codebase/cli.py
@@ -1,13 +1,16 @@
 import os
+import requests
+import tqdm
 
 import fire
-import questionary
 import yaml
 
 from talk_codebase.LLM import factory_llm
 from talk_codebase.consts import DEFAULT_CONFIG
 
 config_path = os.path.join(os.path.expanduser("~"), ".talk_codebase_config.yaml")
+
+DEFAULT_MODEL_URL = "https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
 
 
 def get_config():
@@ -21,29 +24,38 @@ def get_config():
 
 
 def save_config(config):
-    home_dir = os.path.expanduser("~")
     with open(config_path, "w") as f:
         yaml.dump(config, f)
 
 
+def download_model(url, path):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    total_size_in_bytes = int(response.headers.get("content-length", 0))
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+
+    with open(path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            progress_bar.update(len(chunk))
+            f.write(chunk)
+
+    progress_bar.close()
+
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("🤖 ERROR: Something went wrong while downloading the model")
+    else:
+        print(f"🤖 Model downloaded to {path}")
+
+
 def configure():
     config = get_config()
-    model_type = questionary.select(
-        "🤖 Select model type:",
-        choices=[
-            {"name": "OpenAI", "value": "openai"},
-            {"name": "Local", "value": "local"},
-        ]
-    ).ask()
-    config["model_type"] = model_type
-    if model_type == "openai":
-        api_key = input("🤖 Enter your OpenAI API key: ")
-        model_name = input("🤖 Enter your model name (default: gpt-3.5-turbo): ")
-        config["model_name"] = model_name if model_name else DEFAULT_CONFIG["model_name"]
-        config["api_key"] = api_key
-    elif model_type == "local":
-        model_path = input(f"🤖 Enter your model path: (default: {DEFAULT_CONFIG['model_path']}) ")
-        config["model_path"] = model_path if model_path else DEFAULT_CONFIG["model_path"]
+    model_url = input(
+        "🤖 Enter your custom model download URL or press Enter to use default: "
+    )
+    model_url = model_url if model_url else DEFAULT_MODEL_URL
+    config["model_url"] = model_url
+    download_model(model_url, DEFAULT_CONFIG["model_path"])
     save_config(config)
     print("🤖 Configuration saved!")
 
@@ -54,7 +66,7 @@ def loop(llm):
         if not query:
             print("🤖 Please enter a query")
             continue
-        if query in ('exit', 'quit'):
+        if query in ("exit", "quit"):
             break
         llm.send_query(query)
 
@@ -63,16 +75,10 @@ def validate_config(config):
     for key, value in DEFAULT_CONFIG.items():
         if key not in config:
             config[key] = value
-    if config.get("model_type") == "openai":
-        api_key = config.get("api_key")
-        if not api_key:
-            print("🤖 Please configure your API key. Use talk-codebase configure")
-            exit(0)
-    elif config.get("model_type") == "local":
-        model_path = config.get("model_path")
-        if not model_path:
-            print("🤖 Please configure your model path. Use talk-codebase configure")
-            exit(0)
+    model_path = config.get("model_path")
+    if not model_path:
+        print("🤖 Please configure your model path. Use talk-codebase configure")
+        exit(0)
     save_config(config)
     return config
 
@@ -85,15 +91,12 @@ def chat(root_dir):
 
 def main():
     try:
-        fire.Fire({
-            "chat": chat,
-            "configure": configure
-        })
+        fire.Fire({"chat": chat, "configure": configure})
     except KeyboardInterrupt:
         print("\n🤖 Bye!")
     except Exception as e:
         if str(e) == "<empty message>":
-            print("🤖 Please configure your API key. Use talk-codebase configure")
+            print("🤖 Please configure your model path. Use talk-codebase configure")
         else:
             raise e
 
